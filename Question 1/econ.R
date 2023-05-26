f5d225c0d6e3a16ff42cf74fc7f1214cb1cf9b54
FrolickingFigure <maria.teresa.ramos.nogueira@tecnico.ulisboa.pt>
2023-05-26 14:15:21 +0100

    Added geom_smooth

--- Question 1/econ.R
+++ Question 1/econ.R
@@ -20,17 +20,12 @@ for(i in 1:nrow(df)) {
 # Convert to a melted data frame
 meltdf <- melt(df, id="tempo")
 
-# New data frame with the minimum of the two series and the difference
-df$min_val <- pmin(df$tpp, df$ddesemp)
-df$diff_val <- ifelse(df$tpp > df$ddesemp, df$tpp - df$ddesemp, df$ddesemp - df$tpp)
-
 # Create plot
-plot <- ggplot() +
-geom_line(data = meltdf, aes(x=tempo, y=value, colour=variable, group=variable), linewidth=0.5, alpha=0.6) +
-geom_point(data = meltdf, aes(x=tempo, y=value, colour=variable, group=variable), size=0.7) +
-geom_ribbon(data = subset(df, tpp > ddesemp), aes(x=tempo, ymin=min_val, ymax=min_val+diff_val), alpha=0.2, fill="#D65DB1") +
-geom_ribbon(data = subset(df, tpp <= ddesemp), aes(x=tempo, ymin=min_val, ymax=min_val+diff_val), alpha=0.2, fill="#9270D3") +
-scale_color_manual(values=c("#D65DB1", "#9270D3"), labels=c("TPP", "DDesemp")) 
+plot <- ggplot(data = meltdf, aes(x=tempo, y=value, colour=variable, group=variable)) +
+        geom_line(linewidth=0.5, alpha=0.5,na.rm = TRUE) +
+        geom_point(size=0.7, alpha=0.6,na.rm = TRUE) +
+        geom_smooth(na.rm = TRUE, alpha=0.2) +
+        scale_color_manual(values=c("#D65DB1", "#9270D3"), labels=c("TPP", "DDesemp")) 
 
 # Add theming
 lims <- as.POSIXct(strptime(c("1986-01-01","2015-04-01"), format = "%Y-%m-%d"))   
@@ -47,9 +42,10 @@ plot +
                      date_breaks = "25 month",
                      limits = lims,
                      expand = expansion(add = c(0,0))) +
-   scale_y_continuous(labels=scaleFUN, breaks = seq(min(meltdf$value, na.rm = TRUE), max(meltdf$value, na.rm = TRUE), by = 1))
+   scale_y_continuous(labels=scaleFUN, 
+                      breaks = seq(min(meltdf$value, na.rm = TRUE), 
+                                   max(meltdf$value, na.rm = TRUE), by = 1))
 
-print(plot)
 
 
 
