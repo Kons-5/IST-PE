537df349f6dd2086d76a776c0100530f65918427
eusouojoao <81173835+eusouojoao@users.noreply.github.com>
2023-06-03 22:48:46 +0100

    *

--- Question 10/hypothesis_test_ex10.R
+++ Question 10/hypothesis_test_ex10.R
@@ -6,14 +6,16 @@ mu <- 51.9
 sigma <- sqrt(4)  # standard deviation is the square root of variance
 n <- 20
 
-# Generate m = 100 samples of size n from the Normal distribution
-samples <- replicate(100, rnorm(n, mu, sigma))
+# Generate m samples of size n from the Normal distribution
+m <- 100
+samples <- replicate(m, rnorm(n, mu, sigma))
 
 # Conduct a t-test on each sample
 p_values <- apply(samples, 2, function(x) t.test(x, mu = 50.9)$p.value)
 
 # Estimate the probability of not rejecting the null hypothesis
-probability <- mean(p_values > 0.03)
+alpha <- 0.03
+probability <- mean(p_values > alpha)
 
 # Print the estimated probability
 print(round(probability, 3))
